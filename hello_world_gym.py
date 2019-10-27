21980c651a851b571e432d088c87ca193a1f5ede
Kimaru thagana <macbuntu@localhost.localdomain>
2019-10-27 15:12:35 +0300

    use deep learning to create q agent

--- hello_world_gym.py
+++ hello_world_gym.py
@@ -1,5 +1,7 @@
 import gym
 env = gym.make('CartPole-v0') # create environment. Can try out MountainCar-v0, MsPacman-v0, Hopper-v1
+print(env.action_space)
+print(env.observation_space)
 env.reset()
 for _ in range(100000):
     env.render() # display
