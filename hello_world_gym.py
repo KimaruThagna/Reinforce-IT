362bf97576ade60998b673fe75b1f2f02e2c88a2
Kimaru thagana <macbuntu@localhost.localdomain>
2019-10-27 14:50:38 +0300

    add more possible environments

--- hello_world_gym.py
+++ hello_world_gym.py
@@ -1,1 +1,7 @@
 import gym
+env = gym.make('CartPole-v0') # create environment. Can try out MountainCar-v0, MsPacman-v0, Hopper-v1
+env.reset()
+for _ in range(100000):
+    env.render() # display
+    env.step(env.action_space.sample()) # take a random action
+env.close()
